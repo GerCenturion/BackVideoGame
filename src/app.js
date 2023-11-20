02d2c8a28693c25f32cd7efee62918e4e66d31fe
Gerson <gercentu777@gmail.com>
2023-11-20 07:26:19 -0300

    Configutando cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors"); // Agrega esta línea
 const routes = require("./routes/index.js");
 
 const errorHandler = require("./utils/middlewares/errorHandler");
@@ -11,6 +12,9 @@ require("./db.js");
 const server = express();
 
 server.name = "API";
+
+// Configuración del middleware CORS
+server.use(cors());
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
